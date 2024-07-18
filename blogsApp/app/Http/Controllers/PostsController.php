5f1e6cd451d6b6e2a017ddfb474d724c742d7ea1
Siljupg <siljupg9191@gmail.com>
2024-07-18 15:48:11 +1200

    Blogsapp files

--- blogsApp/app/Http/Controllers/PostsController.php
+++ blogsApp/app/Http/Controllers/PostsController.php
@@ -2,24 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
-// class PostsController extends Controller
-// {
-//     public function index ($id) {
-//         $posts = DB::table('posts') ->where('id', $id) ->first();
-//         var_dump($posts);
-//         return view('posts.index');
-//     }
-// }
 
 class PostsController extends Controller
 {
     public function index($id)
     {
-        $posts = DB::table('posts')->where('id', $id)->first();
-        var_dump($posts);
+        // $posts = DB::table('posts')->where('id', $id)->first();
+        $posts = Post::where('id', $id)->first();
+        if(!$posts){
+            abort(404);
+        }
         return view('posts.index', ['post' => $posts]);
     }
 }
